b6fd830eb9ab50ab9a376c3f3622fe0bbc4ed600
Nate <nategamedevelopmentacc@gmail.com>
2021-12-03 23:45:28 -0800

    added color checkerboard

---
+++ src/ColorCheckerBoard.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define CLEAR "\e[1;1H\e[2J"
+#define GRE "\x1b[42m"
+#define RED "\x1b[41m"
+#define YEL "\x1b[43m"
+#define BLU "\x1b[44m"
+#define MAG "\x1b[45m"
+#define RESET "\x1b[0m"
+
+int colorLength = 10;
+int n = 0;
+int i = 0;     //This i can be changed for some crazy effects   
+
+int main()
+{
+    while(1)
+    {
+        for (n = 0; n <= i; n++){
+        printf(RED " "RESET);
+        }
+        for (n = 0; n <= i; n++){
+        printf(GRE " "RESET);
+        }
+        for (n = 0; n <= i; n++){
+        printf(YEL " "RESET);
+        }
+        for (n = 0; n <= i; n++){
+        printf(BLU " "RESET);
+        }
+        for (n = 0; n <= i; n++){
+        printf(MAG " "RESET);
+        }
+
+    }
+
+    return 0;
+}
